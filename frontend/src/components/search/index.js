2e4ed393bf7d9d1013c69769de118d6d03c1e777
Jeroen <jegbol01@gmail.com>
2021-10-05 18:18:51 +0200

    Implemented timeout for searching automatically

--- frontend/src/components/search/index.js
+++ frontend/src/components/search/index.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import SearchIcon from '../../static/icons/search.svg'
 import CloseIcon from '../../static/icons/close.svg'
 import './search.scss'
 
 function Search(props) {
+    const [keyword, setKeyword] = useState('')
+
+    useEffect(() => {
+        const typingTimeOut = setTimeout(
+            search, 500
+        );
+
+        return () => clearTimeout(typingTimeOut);
+    }, [keyword])
+
+    const search = () => {
+        console.log(keyword.target.value)
+    }
+
     return (
         <div className={ 'search' }>
             <img className={ 'searchIcon' } src={ SearchIcon } alt={ 'Search' }/>
-            <input className={ 'searchBar' } type={ 'text' } placeholder={ 'Search' } />
+            <input className={ 'searchBar' } type={ 'text' } placeholder={ 'Search' } onChange={ setKeyword } />
             <div className={ 'close' }>
                 <img className={ 'closeIcon' } src={ CloseIcon } alt={ 'Close' }/>
             </div>
