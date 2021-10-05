7e076f260d77918a1103ea1332f2cfe599458e33
Jeroen <jegbol01@gmail.com>
2021-10-05 23:01:45 +0200

    Added header functionality

--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -2,9 +2,13 @@ import React from 'react'
 import './header.scss'
 
 function Header() {
+    const toTop = () => {
+        window.scrollTo(0, 0)
+    }
+
     return (
         <nav>
-            <h1 id="header-text">
+            <h1 id='header-text' onClick={ toTop }>
                 Kickr
             </h1>
         </nav>
